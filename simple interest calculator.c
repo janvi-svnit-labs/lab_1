bce9d0e8610fd1c802cc5ff3b504ced5464c3039
Janvi Agravat <agravatd9@gmail.com>
2025-09-09 19:26:20 +0530

    Update simple interest calculator.c

--- simple interest calculator.c
+++ simple interest calculator.c
@@ -6,14 +6,21 @@ it uses printf and scanf functions*/
 #include<stdio.h>
 int main()
 {
-    float prncple_amt, rate_of_interest, time, simple_interest;
+    float prncple_amt, rate_of_interest, time, simple_interest; //defining variables
     printf("Enter the principle amount: ");
     scanf("%f", &prncple_amt);
     printf("Enter the annual rate of interest: ");
     scanf("%f", &rate_of_interest);
     printf("Enter the time: ");
     scanf("%f", &time);
-    simple_interest = (prncple_amt*rate_of_interest*time)/100;
-    printf("\nThe simple interest is: %f", simple_interest);
+
+    //handling negative values
+    if (prncple_amt<0 || rate_of_interest<0 || time<0){
+        printf("Enter valid input");
+    }
+    else{
+        simple_interest = (prncple_amt*rate_of_interest*time)/100; //calculations
+        printf("\nThe simple interest is: %f", simple_interest);
+    }
     return 0;
 }
