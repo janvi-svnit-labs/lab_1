0b5e8c327d73dd68f85099a2f96735cf6278fc38
Janvi Agravat <agravatd9@gmail.com>
2025-09-09 19:36:52 +0530

    Update seconds to time converter.c

--- seconds to time converter.c
+++ seconds to time converter.c
@@ -8,11 +8,18 @@ int main()
     int sec, hrs, mins, tailing_sec, remaining_sec;
     printf("Enter the time in seconds: ");
     scanf("%d", &sec);
-    hrs = sec/3600; //converting to hours
-    remaining_sec = sec%3600; //finding remaining seconds
-    mins = remaining_sec/60; //converting the remaining seconds into minutes
-    tailing_sec = remaining_sec%60; //finding the last remaining seconds
-    printf("The time is %d hours %d minutes and %d seconds", hrs, mins, tailing_sec);
+
+    //handling negative values
+    if (sec<0){
+        printf("Enter valid value");
+    }
+    else{
+        hrs = sec/3600; //converting to hours
+        remaining_sec = sec%3600; //finding remaining seconds
+        mins = remaining_sec/60; //converting the remaining seconds into minutes
+        tailing_sec = remaining_sec%60; //finding the last remaining seconds
+        printf("The time is %d hours %d minutes and %d seconds", hrs, mins, tailing_sec);
+    }
     return 0;
 
 }
