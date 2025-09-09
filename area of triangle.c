7a4a43509f37384960b5a2e2842d6418d46a0685
Janvi Agravat <agravatd9@gmail.com>
2025-09-09 15:12:13 +0530

    Update area of triangle.c

--- area of triangle.c
+++ area of triangle.c
@@ -5,13 +5,20 @@ this is a simple code which takes height and length of base as input and calcula
 #include<stdio.h>
 int main()
 {
-    float height, base, area;
+    float height, base, area; //defining variables
     printf("Enter the height of the triangle: ");
     scanf("%f", &height);
     printf("Enter the length of base of triangle: ");
     scanf("%f", &base);
-    area = 0.5 * height * base;
-    printf("\nArea of triangle is: %f", area);
-    return 0;
+
+    //handling 0 or negative values
+    if (height<=0 || base<=0){
+        printf("Enter valid values");
+    }
+    else {
+        area = 0.5 * height * base; //calculation
+        printf("\nArea of triangle is: %f", area);
+        return 0;
+    }
 
 }
